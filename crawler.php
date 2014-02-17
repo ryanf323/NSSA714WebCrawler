c3c03a209aac0f6fdc570f2b55a7cae59acdb91c
ryanf323 <ryanf323@gmail.com>
2014-02-17 15:52:50 -0500

    Big Update
    Big updates today.
    
    -Fixed MySQL insert statement
    
    -Made adaptive output to accomodate html or command line interfaces
    
    -Added statistics to end of output (pages scanned, links in the queue,
    number of matches, time elapsed)

--- crawler.php
+++ crawler.php
@@ -1,33 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Web Crawler - Results</title>
-</head>
-
 <?php
+//Ryan Flynn
 ini_set('max_execution_time', 1800); //30 minutes
 $time_start = microtime(true); 
 include_once('simple_html_dom.php');
 $pagesCounted = 0;
 $date = date("Y-m-d");
-$target_url = $_POST['url'];
+
+//Get URL from command line argument
+if (isset($argv)) {
+    $target_url = $argv[1];
+    $output_type = "cli";
+}
+//Or from a web form!
+else {
+    $target_url = $POST['url'];
+    $output_type = "html";
+    echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Web Crawler - Results</title>
+	</head>';
+}
 $domain_pattern = '/\w+.edu/i';
 preg_match($domain_pattern, $target_url, $matches);
 $domain = $matches[0];
 $match_count = 0;
 $searched_urls = array();
 $not_yet_searched_urls = array($target_url);
-echo "The " . $domain . " Search Results:<p>";
-echo $date . " <br />";
-
-while(count($not_yet_searched_urls) > 0 && $pagesCounted < 100 )
+if ($output_type == "html")
 {
-	search(&$target_url,&$searched_urls,&$not_yet_searched_urls, &$pagesCounted, $date, $domain, &$match_count);
+	echo "The <b>" . $domain . "</b> Search Results:<p>";
+	echo $date . " <br />";
+}else{
+	echo "The " . $domain . " Search Results:\n";
+	echo $date . "\n";
+}
+
+while(count($not_yet_searched_urls) > 0 && $pagesCounted < 100)
+{
+	search(&$target_url,&$searched_urls,&$not_yet_searched_urls, &$pagesCounted, $date, $domain, &$match_count, $output_type);
 	$target_url = array_shift($not_yet_searched_urls);
 }
 
-function search($target_url,&$searched_urls,&$not_yet_searched_urls, &$pagesCounted, $date, $domain, &$match_count){
+function search($target_url,&$searched_urls,&$not_yet_searched_urls, &$pagesCounted, $date, $domain, &$match_count, $output_type){
 
 	$searched_urls[] = $target_url;
 	$html = new simple_html_dom();
@@ -37,23 +53,23 @@ function search($target_url,&$searched_urls,&$not_yet_searched_urls, &$pagesCoun
 
         if (preg_match($pattern, $html))
 		{
-                echo "<a href=\"". $target_url."\">". $target_url ."</a><br />";
-                /*write URL to database
-                $con=mysqli_connect("localhost","db_user","database_name","password");
-                // Check connection
-                if (mysqli_connect_errno())
-                {
-					echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                }
-                mysqli_query($con,"INSERT INTO results (id, url, date) VALUES (NULL, $target_url , $date)");
-                mysqli_close($con);
-				*/
+                if ($output_type == "html")
+		{
+			echo "<a href=\"". $target_url."\">". $target_url ."</a><br />";
+                }else{
+			echo $target_url . "\n";
+		}
+		//write URL to database
+                $mysqli = new mysqli("localhost","username","password","database_name");
+		$target_url = $mysqli->real_escape_string($target_url);
+		$mysqli -> query("INSERT INTO results (id, url, date) VALUES (NULL, '$target_url' , '$date');");
+                $mysqli -> close();
+				
 		$match_count++;
 		}else{
         	//no action
 		}
-	
-	$pagesCounted++;
+$pagesCounted++;
 	
 
 	//define regex for edu links
@@ -63,7 +79,6 @@ function search($target_url,&$searched_urls,&$not_yet_searched_urls, &$pagesCoun
 		$link = $link -> href;
 		if(preg_match($edu, $link) && !already_checked($link, $searched_urls))
 		{
-			//If URL has a / on the end, remove it
 			
 			//add qualifying urls to the search list
 			$not_yet_searched_urls[]=$link;
@@ -83,11 +98,19 @@ function already_checked($reference,$array)
       return false;
 } 
 
-echo "<b>Pages counted: ". $pagesCounted ."<br />";
-echo "Pages left in queue: " . count($not_yet_searched_urls) . "<br />";
-echo "Number of matches: " . $match_count . "</b><br />";
-
 $time_end = microtime(true);
 $execution_time = ($time_end - $time_start);
-echo '<b>Total Execution Time:</b> '.(int)($execution_time/60) .' minutes and '.($execution_time%60).' seconds';
+if ($output_type == "html")
+{
+	echo "<b>Pages scanned: ". $pagesCounted ."<br />";
+	echo "Pages left in queue: " . count($not_yet_searched_urls) . "<br />";
+	echo "Number of matches: " . $match_count . "</b><br />";
+	echo '<b>Total Execution Time:</b> '.(int)($execution_time/60) .' minutes and '.($execution_time%60).' seconds';
+}else{
+        echo "Pages scanned: ". $pagesCounted ."\n";
+        echo "Pages left in queue: " . count($not_yet_searched_urls) . "\n";
+        echo "Number of matches: " . $match_count . "\n";
+        echo 'Total Execution Time: '.(int)($execution_time/60) .' minutes and '.($execution_time%60)." seconds\n";
+
+}
 ?>
